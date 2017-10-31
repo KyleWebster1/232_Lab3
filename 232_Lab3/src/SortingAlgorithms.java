82aaa5c0b8577583c13e3d07e8868ec0cf80b9b0
Kyle Webster <kyle1234webster@gmail.com>
2017-10-31 17:56:48 -0600

    Update SortingAlgorithms.java

--- 232_Lab3/src/SortingAlgorithms.java
+++ 232_Lab3/src/SortingAlgorithms.java
@@ -2,5 +2,20 @@
  * Created by kylewebster1 on 10/30/17.
  */
 public class SortingAlgorithms {
-    
+    AdjacencyMatrix aj;
+    public SortingAlgorithms(AdjacencyMatrix a) {
+        this.aj = a;
+        primsAlgorithm();
+        kruskalsAlgorithm();
+        floydWarshallsAlgorithm();
+    }
+
+    private void primsAlgorithm()
+    {}
+
+    private void kruskalsAlgorithm()
+    {}
+
+    private void floydWarshallsAlgorithm()
+    {}
 }
