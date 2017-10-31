21ae81cf14bd3d7ef7d2a617e61e41fd6e9047ca
Kyle Webster <kyle1234webster@gmail.com>
2017-10-31 15:37:32 -0600

    Update Vertex.java

--- 232_Lab3/src/Vertex.java
+++ 232_Lab3/src/Vertex.java
@@ -2,4 +2,17 @@
  * Created by kylewebster1 on 10/30/17.
  */
 public class Vertex {
-}
+    // Class with constructor for vertexes.
+        private char label; // Names the vertex
+        private boolean wasVisted;
+        public Vertex(char lab){
+            label = lab;
+            wasVisted = false;
+        }
+
+        public char getLabel(){
+            return label;
+        }
+        public boolean getVisited(){return wasVisted;}
+        public void setWasVisted(boolean visited){wasVisted = visited;}
+    }
