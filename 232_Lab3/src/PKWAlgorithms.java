9a422bcaf889e6777059a571622fd741c914ed80
jessejohn01 <jesse.arstein@yahoo.com>
2017-11-24 22:45:53 -0700

    Add files via upload

--- 232_Lab3/src/PKWAlgorithms.java
+++ 232_Lab3/src/PKWAlgorithms.java
@@ -1,1 +1,7 @@
 
+public class PKWAlgorithms {
+    public static void main(String[] args){
+        AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix();
+        SortingAlgorithms sr = new SortingAlgorithms(adjacencyMatrix);
+    }
+}
---
+++ 232_Lab3/src/Vertex.java
@@ -0,0 +1,16 @@
+
+public class Vertex {
+    // Class with constructor for vertexes.
+        private char label; // Names the vertex
+        private boolean wasVisted;
+        public Vertex(char lab){
+            label = lab;
+            wasVisted = false;
+        }
+
+        public char getLabel(){
+            return label;
+        }
+        public boolean getVisited(){return wasVisted;}
+        public void setWasVisted(boolean visited){wasVisted = visited;}
+    }
